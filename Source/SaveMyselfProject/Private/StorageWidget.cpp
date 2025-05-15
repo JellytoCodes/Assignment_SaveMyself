cd8d0a42531a44e4f69593cbd55f47ae96260b70
JellytoCodes <taegu234@naver.com>
2025-05-15 19:50:34 +0900

    250515 창고 구현

--- Source/SaveMyselfProject/Private/StorageWidget.cpp
+++ Source/SaveMyselfProject/Private/StorageWidget.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "StorageWidget.h"
-
+#include "StorageSlot.h"
+#include "Components/WrapBox.h"
--- Source/SaveMyselfProject/Public/StorageWidget.h
+++ Source/SaveMyselfProject/Public/StorageWidget.h
@@ -6,14 +6,17 @@
 #include "Blueprint/UserWidget.h"
 #include "StorageWidget.generated.h"
 
-/**
- * 
- */
+class UStorageSlot;
+class UWrapBox;
 UCLASS()
 class SAVEMYSELFPROJECT_API UStorageWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
 protected :
-	
+	UPROPERTY(EditDefaultsOnly, Category = "StorageSlot")
+	TSubclassOf<UStorageSlot> ItemSlotWidgetClass;
+
+	UPROPERTY(meta = (BindWidget))
+	UWrapBox* StorageWrapBox;
 };
